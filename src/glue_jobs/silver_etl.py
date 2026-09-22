041c23dde66ceccb6b62be6855f336e7f27e2aa7
SharanyaV25-dev <vsharanya00@gmail.com>
2026-09-22 21:15:31 +0530

    gitops ci/cd sync confirmation

--- src/glue_jobs/silver_etl.py
+++ src/glue_jobs/silver_etl.py
@@ -8,6 +8,7 @@ from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
+print("GitOps CI/CD Sync Confirmed - Version 1.0")
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 # trigger CI/CD
 # 1. Initialize Spark with AWS Glue Iceberg Extensions
